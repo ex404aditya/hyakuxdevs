0bc620083c4aa3f0d3e2dd079c280ab22746f837
ex404aditya <ex404ug@gmail.com>
2024-11-01 10:31:40 +0530

    timedcalc

--- assignments-master/01-js/hard/calculator.js
+++ assignments-master/01-js/hard/calculator.js
@@ -16,6 +16,158 @@
   Once you've implemented the logic, test your code by running
 */
 
-class Calculator {}
+class Calculator {
+  constructor() {
+    this.result = 0;
+  }
+
+  add(num) {
+    if (typeof num != "number") {
+      throw new Error("invalid input, pls put a number");
+    }
+    this.result += num;
+    return this.result;
+  }
+
+  subtract(num) {
+    if (typeof num != "number") {
+      throw new Error("invalid input, pls put a number");
+    }
+    this.result -= num;
+    return this.result;
+  }
+
+  multiply(num) {
+    if (typeof num !== "number") {
+      throw new Error("invalid input, pls put a number");
+    }
+    this.result *= num;
+    return this.result;
+  }
+
+  divide(num) {
+    if (typeof num !== "number") {
+      throw new Error("invalid input, pls put a number");
+    }
+    if (num === 0) {
+      throw new Error("division by 0 not allowed");
+    }
+    this.result /= num;
+    return this.result;
+  }
+
+  clear() {
+    this.result =0
+  }
+
+  getResult() {
+    return this.result;
+  }
+
+  calculate(expression) {
+    if (typeof expression !== 'string') {
+        throw new Error('expression is to be string')
+    }
+
+    expression = expression.replace(/\s+/g, '')
+
+    if (!/^[-+().*\/\d]+$/.test(expression)) {
+        throw new Error('contains invalid characters');
+    }
+
+    try {
+        expression = expression.replace(/([-+*/()])/g, ' $1 ')
+                           .trim()
+                           .split(/\s+/);
+
+        return this.evaluateExpression(expression);
+    } catch (error) {
+        throw new Error('invalid expression: ' + error.message);
+    }
+}
+
+evaluateExpression(tokens) {
+  const outputQueue = [];
+  const operatorStack = [];
+  const precedence = {
+      '+': 1,
+      '-': 1,
+      '*': 2,
+      '/': 2
+  };
+
+  for (let i = 0; i < tokens.length; i++) {
+      const token = tokens[i];
+
+      if (!isNaN(token)) {
+          outputQueue.push(Number(token));
+      } else if (token in precedence) {
+          while (
+              operatorStack.length > 0 &&
+              operatorStack[operatorStack.length - 1] !== '(' &&
+              precedence[operatorStack[operatorStack.length - 1]] >= precedence[token]
+          ) {
+              outputQueue.push(operatorStack.pop());
+          }
+          operatorStack.push(token);
+      } else if (token === '(') {
+          operatorStack.push(token);
+      } else if (token === ')') {
+          let foundMatching = false;
+          while (operatorStack.length > 0) {
+              const operator = operatorStack.pop();
+              if (operator === '(') {
+                  foundMatching = true;
+                  break;
+              }
+              outputQueue.push(operator);
+          }
+          if (!foundMatching) {
+              throw new Error('mismatched parentheses');
+          }
+      }
+  }
+
+  while (operatorStack.length > 0) {
+      const operator = operatorStack.pop();
+      if (operator === '(') {
+          throw new Error('mismatched parentheses');
+      }
+      outputQueue.push(operator);
+  }
+
+  const evaluationStack = [];
+  for (const token of outputQueue) {
+      if (typeof token === 'number') {
+          evaluationStack.push(token);
+      } else {
+          const b = evaluationStack.pop();
+          const a = evaluationStack.pop();
+          if (a === undefined || b === undefined) {
+              throw new Error('invalid expression');
+          }
+
+          switch (token) {
+              case '+': evaluationStack.push(a + b); break;
+              case '-': evaluationStack.push(a - b); break;
+              case '*': evaluationStack.push(a * b); break;
+              case '/':
+                  if (b === 0) throw new Error('division by zero');
+                  evaluationStack.push(a / b);
+                  break;
+          }
+      }
+  }
+
+  if (evaluationStack.length !== 1) {
+      throw new Error('invalid expression');
+  }
+
+  this.result = evaluationStack[0];
+  return this.result;
+
+}
+
+}
 
 module.exports = Calculator;
